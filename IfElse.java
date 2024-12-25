dd12e3c3fcd44b1d73f2423e8d333cc626840bc4
Tanushree227 <tanushreedas813@gmail.com>
2024-12-25 12:39:03 +0530

    Checking a number is odd or even in Java

--- IfElse.java
+++ IfElse.java
@@ -3,16 +3,17 @@ import java.util.Scanner;
 
 public class IfElse {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter your age: ");
-        int age = sc.nextInt();
-        if(age > 18)
-        {
-            System.out.println("You are an adult. And, you are eligible to vote");
-        }
-        else
-        {
-            System.out.println("You are not an adult. You are not eligible to vote");
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Enter your age: ");
+            int age = sc.nextInt();
+            if(age > 18)
+            {
+                System.out.println("You are an adult. And, you are eligible to vote");
+            }
+            else
+            {
+                System.out.println("You are not an adult. You are not eligible to vote");
+            }
         }
     }
 }
