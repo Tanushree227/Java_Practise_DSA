77fd5394bd4982622f110ea4b6ddd8f9d3907382
Tanushree227 <tanushreedas813@gmail.com>
2024-12-25 13:16:13 +0530

    looping statements

--- PrintNumber.java
+++ PrintNumber.java
@@ -1,8 +1,29 @@
 public class PrintNumber {
     public static void main(String[] args) {
+        //for loop
+        System.out.println("Using For loop");
         for(int i = 0; i <= 10; i++)
         {
             System.out.println(i);
         }
+
+        //while loop
+        int i = 0;
+        System.out.println("Using While loop");
+        while(i <= 10)
+        {
+            System.out.println(i);
+            i++;
+        }
+
+        //do-while loop
+        int j = 0;
+        System.out.println("Using Do-While loop");
+        do
+        {
+            System.out.println(j);
+            j++;
+        } while(j<=10);
+
     }
 }
