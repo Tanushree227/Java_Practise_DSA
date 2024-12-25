03a960b3d9a4e59b295f75d468c887a8da56ec6f
Tanushree227 <tanushreedas813@gmail.com>
2024-12-25 13:23:09 +0530

    try with resources

--- Sum.java
+++ Sum.java
@@ -2,14 +2,15 @@ import java.util.Scanner;
 
 public class Sum {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        try (Scanner sc = new Scanner(System.in)) {
+            int n = sc.nextInt();
 
-        int sum = 0;
-        for(int i = 1; i <= n; i++)
-        {
-            sum = sum + i;
+            int sum = 0;
+            for(int i = 1; i <= n; i++)
+            {
+                sum = sum + i;
+            }
+            System.out.println("Sum: " +sum);
         }
-        System.out.println("Sum: " +sum);
     }
 }
